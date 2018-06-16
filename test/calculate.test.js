17ca615c1ae3f7624648e9a02e9445d6b8ae6a1c
benspier <benspier@gmail.com>
2018-06-15 22:01:59 -0400

    update calculate test

--- test/calculate.test.js
+++ test/calculate.test.js
@@ -1,13 +1,5 @@
 import test from 'ava';
-
-const calculate = (n1, operator, n2) => {
-  const firstNum = parseFloat(n1);
-  const secondNum = parseFloat(n2);
-  if (operator === 'add') return firstNum + secondNum;
-  if (operator === 'subtract') return firstNum - secondNum;
-  if (operator === 'multiply') return firstNum * secondNum;
-  if (operator === 'divide') return firstNum / secondNum;
-};
+import { calculate } from '../src/components/calculate';
 
 test('adds 2 numbers', t => {
   t.is(calculate(1, 'add', 2), 3);
